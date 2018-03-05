083f3592b57535fc82a8828f5fa8e7b70a3fdbf5
Robert Khousadian <31484519+rkhous@users.noreply.github.com>
2018-03-05 12:12:55 -0800

    ignore all channels except alerts

--- alerts.py
+++ alerts.py
@@ -9,7 +9,8 @@ bot = commands.Bot(command_prefix='.') #add prefix, although it won't be used
 async def on_message(message):
     if message.author == bot.user: #don't allow the bot to reply to it's own message
         return
-    if random.randint(1,1) == 1: #reply on any message
+    alerts_channel = 'Channel ID here' #alerts channel ID, ignore everything else. 
+    if 1 == 1 and message.channel.id == alerts_channel: #reply on any message and limit to alerts channel
         await bot.send_message(discord.Object(id='Channel ID here'), '*Alerts spawn successfully recognized, beginning spam.*')
         await asyncio.sleep(1)
         spam = 0
